a8c64a6620417e21d4864c56ca381b7bccc37618
evgendn <evgeniydein@gmail.com>
2016-12-27 00:56:17 +0500

    Add insert query and fix create query

--- queries/create_tables.sql
+++ queries/create_tables.sql
@@ -1,32 +1,30 @@
---Need at least 1 table and create db!
-
 create table driver (
 	id serial,
-	full_name(128) not null,
+	full_name varchar(128) not null,
 	birth_date date not null,
-	driving_licence_number varchar(10) not null,
+	driving_licence_number varchar(10),
 	expire_date date not null,
-	working boolean not null default(0),
+	working boolean not null default(false),
 
 	primary key(id),
 	constraint valid_licence_number check(
-		driving_licence_number ~ "^[0-9]|[a-zA-Z]{10}$")
+		driving_licence_number ~ '^([0-9]|[a-zA-Z]){10}$')
 );
 
 create table car_model (
 	id serial,
 	model_name varchar(25) not null,
-	model_description text null
+	model_description text null,
 
 	primary key (id)
 );
 
 create table cab (
 	id serial,
-	licence_plate varchar(6) not null,
+	license_plate varchar(6) not null,
 	car_model_id int not null,
 	owner_id int null,
-	active boolean not null default(0),
+	active boolean not null default(false),
 
 	primary key(id),
 	foreign key(car_model_id) references car_model(id),
@@ -39,12 +37,10 @@ create table shift (
 	cab_id int not null,
 	shift_start_time time not null,
 	shift_finish_time time not null,
-	login_time time null,
-	logout_time time null
 
 	primary key(id),
 	foreign key(driver_id) references driver(id),
-	foreign key(car_id) references cab(id)
+	foreign key(cab_id) references cab(id)
 );
 
 create type payment as enum (
@@ -60,14 +56,14 @@ create table payment_type (
 -- Call center agent
 create table cc_agent (
 	id serial, 
-	first_name varchar(25) null,
-	last_name varchar(25) null,
+	first_name varchar(25),
+	last_name varchar(25),
 
 	primary key(id),
 	constraint valid_first_name check(
-		first_name ~ "^[a-zA-Z]{1-25}$"),
+		first_name ~ '[a-zA-Z]+'),
 	constraint valid_last_name check(
-		last_name ~ "^[a-zA-Z]{1-25}$")
+		last_name ~ '[a-zA-Z]+')
 );
 
 create type state as enum(
@@ -80,21 +76,35 @@ create table status (
 	primary key(id)
 );
 
+create table customer (
+	id serial,
+	email varchar(50) not null,
+	password varchar(64) not null,
+	credit_card_number varchar(16) null,
+
+	primary key(id)
+	-- Не пашет регулярка в базе, в дебаггере норм работает!
+	-- constraint ck_validate_email check (
+	-- 	email ~ '^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')
+);
+
 create table cab_ride (
 	id serial,
-	shift_id int null,
+	shift_id int,
+	customer_id int not null,
 	ride_start_time time null,
 	ride_end_time time null,
-	address_starting_pint text null,
+	address_starting_point text null,
 	gps_starting_point text null,
-	address_destionation text null,
+	address_destination text null,
 	gps_destination text null,
 	canceled boolean,
-	payment_type_id int,
+	payment_type_id int not null,
 	price numeric(4, 2) null,
 
 	primary key(id),
 	foreign key(shift_id) references shift(id),
+	foreign key(customer_id) references customer(id),
 	foreign key(payment_type_id) references payment_type(id)
 );
 
---
+++ queries/insert_tables.sql
@@ -0,0 +1,152 @@
+insert into driver (
+		full_name,
+		birth_date,
+		driving_licence_number,
+		expire_date,
+		working
+	) values
+		('John Doe', '1985-07-10', '10ej38ru82', '2018-10-01', true),
+		('Jerald Broflovski', '1970-12-04', '13ks02lmsu', '2021-12-24', true),
+		('Alex King', '1964-04-18', 'jdyu492nx8', '2018-07-14', true),
+		('Sam Ganji', '1978-08-23', '7yhj03jsl4', '2017-03-26', true),
+		('Victor Reznov', '1983-03-03', 'aj37nsi3m3', '2019-06-12', true),
+		('Aizek Clark', '1992-06-11', '10ej38ur38', '2020-05-22', true);
+
+
+insert into car_model (
+		model_name,
+		model_description
+	) values
+		('Ford focus', 'blue'),
+		('Toyota prius', 'San-Francisco Sticker'),
+		('Ford focus', 'blue'),
+		('BMW cabrio', 'cabriolet'),
+		('HAMMER', 'with stupid back label'),
+		('Tesla', ''),
+		('Mustang', 'green'),
+		('Audi x5', 'red');
+
+
+insert into cab (
+		license_plate,
+		car_model_id,
+		owner_id,
+		active
+	) values 
+		('23jj2f', 3, 2, true),
+		('90er4b', 2, 2, true),
+		('u73ns0', 4, 4, true),
+		('snj30j', 6, 5, true),
+		('nz30m3', 8, 6, false);
+
+
+insert into shift (
+		driver_id, 
+		cab_id,
+		shift_start_time,
+		shift_finish_time
+	) values
+		(4, 3, '09:00:00', '17:30:00'),
+		(2, 1, '12:00:00', '20:00:00'),
+		(6, 5, '01:30:00', '08:45:00'),
+		(6, 5, '07:00:00', '15:00:00'),
+		(5, 4, '10:00:00', '13:00:00');
+
+
+insert into payment_type (
+		type_name
+	) values 
+		('cash'),
+		('credit card'),
+		('bonuses');
+
+
+insert into cc_agent (
+		first_name,
+		last_name
+	) values 
+		('Bob', 'Burnwood'),
+		('Anton', 'MacGee'),
+		('Jason', 'Lee'),
+		('Frank', 'Corn'),
+		('Matt', 'Stone');
+
+
+insert into status (
+		status_name
+	) values
+		('new'),
+		('assigned'),
+		('started'),
+		('ended'),
+		('canceled');
+
+
+insert into customer (
+		email,
+		password,
+		credit_card_number
+	) values
+		('qwer2020@mail.com', 'ksdhbfbsldkfe', '1111111111111111'),
+		('sadmanhello@mail.com', 'akkhhej394mld', '2222222222222222'),
+		('candy93@mail.com', '324nsadlij465', '3333333333333333'),
+		('house13@ya.com', 'ndfjdfdflkj91', '4444444444444444'),
+		('alex@ml.com', 'drnejflicjdlr', '5555555555555555'),
+		('updown@mail.com', 'n43od94nkc904', '6666666666666666'),
+		('setlink@mail.com', 'nklfjh93no40e', '7777777777777777'),
+		('dleknr@mail.com', '34lji@34we534', '8888888888888888'),
+		('summer@gmail.com', '4jbf9503kck30', '9999999999999999');
+
+
+insert into cab_ride (
+		shift_id,
+		customer_id,
+		ride_start_time,
+		ride_end_time,
+		address_starting_point,
+		gps_starting_point,
+		address_destination,
+		gps_destination,
+		canceled,
+		payment_type_id,
+		price
+	) values
+		(1, 3, '12:53:42', '13:15:34',
+			'linkoln 14th', '239082403872', 
+			'marystreet 83th', '232324872643',
+			false, 1, '15'),
+
+		(2, 1, '03:05:42', '03:45:34',
+			'subway 93th', '934834035544', 
+			'brooklin str 83th', '9495029203',
+			false, 2, '35'),
+
+		(3, 3, '12:53:42', '13:15:34',
+			'marystreet 83th', '232324872643', 
+			'linkoln 14th', '239082403872',
+			false, 1, '15'),
+
+		(4, 5, '20:23:42', '20:52:34',
+			'greenbay 98th', '485064058956', 
+			'broadway 3th', '187239834094',
+			true, 3, '32'),
+
+		(5, 9, '21:53:42', '22:13:34',
+			'redcapes 34th', '342343403872', 
+			'marystreet 83th', '232324872643',
+			false, 2, '8');
+
+
+insert into cab_ride_status (
+		cab_ride_id,
+		status_id,
+		status_time,
+		cc_agent_id,
+		shift_id,
+		status_details
+	) values 
+		(1, 3, '12:53:42', 5, 1, ''),
+		(2, 1, '02:55:12', 4, 2, '3 person'),
+		(3, 2, '12:49:31', 3, 3, ''),
+		(4, 5, '20:52:34', 2, 4, 'drunk customer'),
+		(5, 4, '22:13:34', 1, 5, 'ok');
