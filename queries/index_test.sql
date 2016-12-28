8e774ef3a33283bdbd2bbbe0d21a58c851c99d87
evgendn <evgeniydein@gmail.com>
2016-12-28 05:58:24 +0500

    Finish index part

--- queries/index_test.sql
+++ queries/index_test.sql
@@ -1,7 +1,7 @@
-drop table test_shit_cab_rides;
+drop table test_cab_rides;
 
 create table test_cab_rides (
-	id serial,
+	id int,
 	shift_id int,
 	customer_id int not null,
 	ride_start_time timestamp null,
@@ -12,52 +12,34 @@ create table test_cab_rides (
 	gps_destination text null,
 	canceled boolean,
 	payment_type_id int not null,
-	price numeric(4, 2) null,
-
-	primary key(id),
-	foreign key(shift_id) references shift(id),
-	foreign key(customer_id) references customer(id),
-	foreign key(payment_type_id) references payment_type(id)
+	price numeric(4, 2) null
 );
 
 insert into test_cab_rides (
+		id,
 		shift_id,
 		customer_id,
 		canceled,
 		payment_type_id,
 		price
-	) select i, i, false, 1, 40.00
+	) select i, random()*(100-1)+1, random()*(500-1)+1, false, 1, 40.00
 		from generate_series(0, 999999) as i;
 
+-- Без индексов.
+select * from test_cab_rides; 
+-- Время: 1129,458 мс.
+select * from test_cab_rides where shift_id = 100; 
+-- Время: 152,385 мс.
 
-drop table test_cab_ride_status;
+-- В postgres нету кластерных ключей как таковых
+-- используется CLUSTER, которого нету в стандарте SQL.
+create index index_id on test_cab_rides(id);
+cluster test_cab_rides using index_id;
 
-create table test_cab_ride_status (
-	id serial,
-	cab_ride_id int not null,
-	status_id int not null,
-	status_time timestamp default(current_timestamp),
-	cc_agent_id int null,
-	shift_id int null,
-	status_details text null,
+create index index_shift_id on test_cab_rides(shift_id);
 
-	primary key(id),
-	foreign key(cab_ride_id) references cab_ride(id),
-	foreign key(status_id) references status(id),
-	foreign key(cc_agent_id) references cc_agent(id),
-	foreign key(shift_id) references shift(id)
-);
-
-
-insert into test_cab_ride_status (
-		cab_ride_id,
-		status_id,
-		cc_agent_id,
-		shift_id,
-		status_details
-	) select 1, 2, 1, 3, i::text
-		from generate_series(0, 999999) as i;
-
-
--- Индексы будут далее.
-
+-- С индексами.
+select * from test_cab_rides; 
+-- Время: 1050,400 мс.
+select * from test_cab_rides where shift_id = 100; 
+-- Время: 32,999 мс.
